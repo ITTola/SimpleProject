074bee53619ccb16462feb59ff64d8f0b6efe79f
Tona <Tona@DESKTOP-TKEOP92>
2017-10-01 14:02:52 +0700

    new commit

---
+++ src/org/ainsoft/services/StudentService.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ainsoft.services;
+
+import java.sql.SQLException;
+import org.ainsoft.model.Student;
+
+/**
+ *
+ * @author Tona
+ */
+public interface StudentService {
+    java.util.List<Student> getStudents()throws SQLException;
+    Student getStudentByName(String studentName)throws SQLException;
+}
